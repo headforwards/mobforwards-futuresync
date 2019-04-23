ed51ee0fee091c65bff349db874d010293f04528
Barney Nicholls <barney.nicholls@bluetoad.net>
2019-04-23 17:03:46 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="container">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col">
+            <h1>The Challenge</h1>
+            <p>We are going to build a useful app by using the power of the mob</p>
+            <p>View our progress throughout the day by visiting</p>
+            <h3>https://mobforwards.azurewebsites.net</h3>
+          </div>
+          <div className="col"></div>
+        </div>
+      </div>
+    );
+    
+  }
+}
+
+export default App;
