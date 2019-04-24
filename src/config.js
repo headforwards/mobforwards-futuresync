a3fb96d8829f32e7506dfe85a4da97348b2c1732
Paul <paul.donovan@outlook.com>
2019-04-24 15:47:47 +0100

    missed files

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+const config = {
+    apiKey: "AIzaSyAbEmZk8AWgZVUQWMLdnADIY8kfXJCAvRE",
+    authDomain: "futuresync2019.firebaseapp.com",
+    databaseURL: "https://futuresync2019.firebaseio.com",
+    projectId: "futuresync2019",
+    storageBucket: "futuresync2019.appspot.com",
+    messagingSenderId: "622766030327"
+};
+
+export default config;
