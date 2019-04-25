61d98a9d788c5ced8d88b0fa8732b85cfdbade71
Barney Nicholls <barney.nicholls@bluetoad.net>
2019-04-25 12:00:31 +0100

    first set of tests :)

--- src/App.js
+++ src/App.js
@@ -1,45 +1,30 @@
 import React, { Component } from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import config from './config';
 import Firebase from 'firebase';
+import config from './config';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   
   constructor(props) {
     super(props);
     Firebase.initializeApp(config);
-this.state = {}
-  }
 
-  getSpeakerData = () => {
-    let ref = Firebase.database().ref('/speakers');
-    ref.on('value', snapshot => {
-      const state = snapshot.val();
-      this.setState({ speakers : state});
-    });
-  }
-
-  componentDidMount() {
-    this.getSpeakerData();
+    this.state = {};
   }
 
   render() {
     
-    const { speakers } = this.state;
-    console.log(speakers);
     return (
 
       <div className="container">
         <div className="row">
           <div className="col"></div>
           <div className="col">
-            <h1>The Challenge</h1>
-            <p>We are going to build a useful app by using the power of the mob</p>
-            <p>View our progress throughout the day by visiting</p>
-            <h3>https://futuresync2019mob.azurewebsites.net/</h3>
+            We now have 3 tests passing and connected to firebase :)
           </div>
           <div className="col">
-          {speakers && speakers.length > 0 && speakers[2].name }
+        
           </div>
           
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import './setupTests';
+import Firebase from 'firebase';
+
+jest.mock('firebase', () => {
+  return {
+    initializeApp: jest.fn()
+  }
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('initalizes the database connection correctly', () => {
+  expect(Firebase.initializeApp).toHaveBeenCalled();
+});
+
+it('sets the state', () => {
+  const app = shallow(<App />);
+  expect(app.state()).toMatchObject({});
+});
+
