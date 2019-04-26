6e55f5b8b12c825c091df5a2f4ffe39c4185551e
Barney Nicholls <barney.nicholls@axa-ppp.co.uk>
2019-04-26 12:25:31 +0100

    Update styling

--- src/Speaker.js
+++ src/Speaker.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './Speaker.css'
 
-const Speaker = ({speaker}) => {
+const Speaker = ({ speaker }) => {
     return <div className="row padme">
-      <div class="col-3"><img src={speaker.imageUrl} data-image alt={speaker.name} />
-      </div>
-       <div class="col-9">
-       
-                        <div data-name>
-                            <h3>{speaker.name}</h3>
-                        </div>
-                        <div data-title>
-                            <h4>{speaker.talk.title}</h4>
-                            <p data-description>{speaker.talk.description}</p>
-                        </div>
-                        <div>
-                            <button className="btn btn-success" data-interest >I'm interested</button>
-                        </div>
-             </div>
-             <hr/>
-</div>
+        <div class="col-3"><img src={speaker.imageUrl} class="img-fluid" data-image alt={speaker.name} />
+        </div>
+        <div class="col-9">
+
+            <div data-name>
+                <h3>{speaker.name}</h3>
+            </div>
+            <div data-title>
+                <h4>{speaker.talk.title}</h4>
+                <p data-description>{speaker.talk.description}</p>
+            </div>
+            <div>
+                <button className="btn btn-success" data-interest >I'm interested</button>
+            </div>
+        </div>
+        <hr />
+    </div>
 
 };
 
